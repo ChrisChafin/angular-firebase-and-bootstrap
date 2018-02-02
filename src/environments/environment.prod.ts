383b8e279a3314f10cdd5f09144c590b1678d33f
ChrisChafin <chris182@verizon.net>
2018-02-02 13:40:05 -0700

    Adding Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBBsO0JRaI0rw3x2qHz3WZrB5muQBZerw8",
+    authDomain: "angular-firebase-and-bootstrap.firebaseapp.com",
+    databaseURL: "https://angular-firebase-and-bootstrap.firebaseio.com",
+    projectId: "angular-firebase-and-bootstrap",
+    storageBucket: "angular-firebase-and-bootstrap.appspot.com",
+    messagingSenderId: "439965902957"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBBsO0JRaI0rw3x2qHz3WZrB5muQBZerw8",
+    authDomain: "angular-firebase-and-bootstrap.firebaseapp.com",
+    databaseURL: "https://angular-firebase-and-bootstrap.firebaseio.com",
+    projectId: "angular-firebase-and-bootstrap",
+    storageBucket: "angular-firebase-and-bootstrap.appspot.com",
+    messagingSenderId: "439965902957"
+  }
 };
